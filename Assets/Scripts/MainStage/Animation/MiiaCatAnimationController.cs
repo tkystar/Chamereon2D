9c1885441b9701d5c51dad209dab4e29a9d71f61
Bontarou7 <imagine77er@gmail.com>
2021-05-02 22:51:31 +0900

    未追跡のファイルをコミット

---
+++ Assets/Scripts/MainStage/Animation/MiiaCatAnimationController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiiaCatAnimationController : MonoBehaviour
+{
+    Animator animator;
+    int n = 0;
+    float span;
+    bool ani;
+    private Vector2 input;   
+    [SerializeField] private float SPEED = 3.5f;
+    Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        ani = true;
+        rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ani)
+        {
+            ani = false;
+
+            if (n == 0)     ///stay
+            {
+                animator.SetInteger("Transition", 0);               
+                StartCoroutine("backtransition");
+
+            }
+            else if (n == 1)      ///right
+            {
+                animator.SetInteger("Transition", 1);              
+                StartCoroutine("backtransition");
+            }
+            else if (n == 2)     ///left
+            {
+                animator.SetInteger("Transition", 2);                
+                StartCoroutine("backtransition");
+            }
+            else if (n == 3)      ///eye
+            {
+                animator.SetInteger("Transition", 3);               
+                StartCoroutine("backtransition");
+            }
+
+
+           
+        }
+    }
+
+    private IEnumerator backtransition()
+    {
+       
+        yield return new WaitForSeconds(3.0f);
+        n = Random.Range(0, 4);
+        ani = true;
+    }
+}
---
+++ Assets/Scripts/MainStage/House/ExitHouse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Western
+{
+
+    public class ExitHouse : MonoBehaviour
+    {
+        public GameObject chamereon;
+        public GameObject spawnPosObj;
+        Vector3 _backPos;
+        HouseManager housemanager;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _backPos = spawnPosObj.GetComponent<Transform>().position;
+            this.GetComponent<CircleCollider2D>().enabled = false;
+            housemanager=spawnPosObj.GetComponent<HouseManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                chamereon.transform.position = _backPos;
+                spawnPosObj.GetComponent<BoxCollider2D>().enabled = false;
+                housemanager.BackEnterHousePos();
+                
+            }
+        }
+
+
+    }
+    
+
+}
+    
+
