0ed67d66078c7b066b7d15b4f64525e2d4eca5f6
tkystar <tky.star.1231@gmail.com>
2021-05-02 15:01:49 +0900

    add:アニメーション

--- Assets/Scripts/MiniGame04/JustMove.cs
+++ Assets/Scripts/MiniGame04/JustMove.cs
@@ -6,7 +6,7 @@ public class JustMove : MonoBehaviour
 {
     private Rigidbody2D rb;
     public GameObject Camereon;
-    public int Speed;
+    [SerializeField] private int Speed;   
     public Sprite left;
     public Sprite right;
     public Sprite up;
@@ -14,13 +14,14 @@ public class JustMove : MonoBehaviour
     public GameObject player;
     BoxCollider2D collider;
     Vector3 playertransform;
-
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         collider = this.gameObject.GetComponent<BoxCollider2D>();
         playertransform=player.GetComponent<Transform>().transform.localScale;
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,7 +30,7 @@ public class JustMove : MonoBehaviour
         if (Input.GetKey("left"))
         {
             rb.velocity = new Vector2(-Speed, 0);
-
+            animator.SetInteger("camereonTransition", 2);
 
             //hoge.sizeDelta = new Vector2(x2, y2);
 
@@ -37,7 +38,7 @@ public class JustMove : MonoBehaviour
         else if (Input.GetKey("right"))
         {
             rb.velocity = new Vector2(Speed, 0);
-
+            animator.SetInteger("camereonTransition", 3);
 
             //hoge.sizeDelta = new Vector2(x2, y2);
 
@@ -45,7 +46,7 @@ public class JustMove : MonoBehaviour
         else if (Input.GetKey("up"))
         {
             rb.velocity = new Vector2(0, Speed);
-
+            animator.SetInteger("camereonTransition", 0);
 
             //hoge.sizeDelta = new Vector2(y1, x1);
 
@@ -53,7 +54,7 @@ public class JustMove : MonoBehaviour
         else if (Input.GetKey("down"))
         {
             rb.velocity = new Vector2(0, -Speed);
-
+            animator.SetInteger("camereonTransition", 1);
             //hoge.sizeDelta = new Vector2(y1, x1);
 
         }
@@ -63,7 +64,7 @@ public class JustMove : MonoBehaviour
         if (Input.GetKeyUp("left"))
         {
             rb.velocity = new Vector2(0, 0);
-
+            animator.SetInteger("camereonTransition", 6);
 
             //hoge.sizeDelta = new Vector2(x2, y2);
 
@@ -71,7 +72,7 @@ public class JustMove : MonoBehaviour
         else if (Input.GetKeyUp("right"))
         {
             rb.velocity = new Vector2(0, 0);
-
+            animator.SetInteger("camereonTransition", 7);
 
             //hoge.sizeDelta = new Vector2(x2, y2);
 
@@ -79,7 +80,7 @@ public class JustMove : MonoBehaviour
         else if (Input.GetKeyUp("up"))
         {
             rb.velocity = new Vector2(0, 0);
-
+            animator.SetInteger("camereonTransition", 4);
 
             //hoge.sizeDelta = new Vector2(y1, x1);
 
@@ -87,11 +88,11 @@ public class JustMove : MonoBehaviour
         else if (Input.GetKeyUp("down"))
         {
             rb.velocity = new Vector2(0, 0);
-
+            animator.SetInteger("camereonTransition", 5);
             //hoge.sizeDelta = new Vector2(y1, x1);
         }
 
-
+        /*
         if (Input.GetKeyDown("left"))
         {
 
@@ -121,6 +122,7 @@ public class JustMove : MonoBehaviour
             player.GetComponent<Transform>().transform.localScale = new Vector3(1, 1, 1);
             collider.size = new Vector2(3, 6);
         }
+        */
 
 
 
